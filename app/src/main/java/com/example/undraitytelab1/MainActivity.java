7739c5cdc9e062f429ab014f28b2186de00ab198
Kotryna <kotryna.undraityte@knf.stud.vu.lt>
2021-04-18 19:40:35 +0300

    changed text

--- app/src/main/java/com/example/undraitytelab1/MainActivity.java
+++ app/src/main/java/com/example/undraitytelab1/MainActivity.java
@@ -2,12 +2,24 @@ package com.example.undraitytelab1;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvMain;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.tvMain = findViewById(R.id.tvMain);
+    }
+
+    public void onbtnclick(View view) {
+
+        this.tvMain.setText(R.string.changed_label_caption);
     }
 }
